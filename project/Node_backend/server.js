c5570a632d4552ca57c2ae98aa324c1a0746643f
Hassan <victorymachine111@gmail.com>
2022-02-18 15:56:32 +0500

    Added socket.io library and some base functionality (Hassan)

--- project/Node_backend/server.js
+++ project/Node_backend/server.js
@@ -1,5 +1,7 @@
 const express = require("express");
+var socket = require('socket.io');
 const app = express();
+
 
 app.use(express.json());
 // const port = 5000
@@ -19,6 +21,26 @@ app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
-app.listen(port, () => {
+var server = app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
+});
+
+// app.use(express.static('public'));
+
+var io = socket(server);
+io.on('connection', (socket) => {
+
+  console.log('made socket connection', socket.id);
+
+  // Handle chat event
+  socket.on('chat', function (data) {
+    // console.log(data);
+    io.sockets.emit('chat', data);
+  });
+
+  // Handle typing event
+  socket.on('typing', function (data) {
+    socket.broadcast.emit('typing', data);
+  });
+
 });
