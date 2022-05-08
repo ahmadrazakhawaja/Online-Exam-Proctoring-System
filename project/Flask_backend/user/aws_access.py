3aa6cc6fe4d7c108a7fb9b5004b755bab0cddf85
ahmadrazakhawaja <ahmadrazakhawaja@gmail.com>
2022-05-08 17:00:05 +0500

    removed file containing secrets

--- project/Flask_backend/user/aws_access.py
+++
@@ -1,6 +0,0 @@
-import os
-
-def get_aws():
-    os.environ["AWS_DEFAULT_REGION"] = 'us-east-1'
-    os.environ["AWS_ACCESS_KEY_ID"] = 'AKIAX5H7BEGKNPK2SKLT'
-    os.environ["AWS_SECRET_ACCESS_KEY"] = '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'
